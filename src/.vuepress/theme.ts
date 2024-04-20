41be08d8ad5a20d1d21709c489618613d21dd999
liShuangQ-pc <li_shuangq@foxmail.com>
2024-04-20 23:13:55 +0800

    优化配置

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: "",
 
   author: {
     name: "Mr.Shuangqi",
@@ -14,7 +14,7 @@ export default hopeTheme({
 
   logo: "",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "",
 
   docsDir: "src",
 
